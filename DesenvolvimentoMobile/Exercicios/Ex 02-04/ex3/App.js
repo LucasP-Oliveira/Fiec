6b59f4bb1b96962166d339968f910b1f8634ba41
Lucas37406 <37406@fiecalunos.lan>
2025-04-09 19:05:48 -0300

    changing some changes in the projects

--- DesenvolvimentoMobile/Exercicios/Ex 02-04/ex3/App.js
+++ DesenvolvimentoMobile/Exercicios/Ex 02-04/ex3/App.js
@@ -5,7 +5,7 @@ import { Header } from './Components/Header.js';
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
-      <Header title="Header" />
+      <Header title="Meu App" />
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       </View>
     </View>
